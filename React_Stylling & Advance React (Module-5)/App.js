056fec71078ef8953ca72ad7ad4484a4479510e0
Janvi Panchal <110367464+Janvipanchal0609@users.noreply.github.com>
2024-04-17 20:42:05 +0530

    Update App.js

--- React_Stylling & Advance React (Module-5)/App.js
+++ React_Stylling & Advance React (Module-5)/App.js
@@ -2,7 +2,7 @@ import { Shop } from "@mui/icons-material";
 import Class_component from "./Component/Class_component";
 import Fun_component from "./Component/Func_component";
 import Home from "./Layout/Pages/Home";
-import Shopping_web from "./MUI/Shopping_web";
+import Shopping_web from "./MUI/Shopping";
 import { Helmet } from "react-helmet";
 import Listview from "./Lists/Listview";
 import Cart from "./Lists/Cart";
